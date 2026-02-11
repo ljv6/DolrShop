784934223098626f6a37c0e5b800084f789f3701
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 02:42:21 +0300

    Update banners.js

--- banners.js
+++ banners.js
@@ -1,6 +1,14 @@
-const bannerConfig = {
-    title: "DOLR PLUS",
-    subtitle: "Luxury Digital Services",
-    image: "https://images.unsplash.com/photo-1614850523296-d8c1af93d400?q=80&w=2070",
-    opacity: "opacity-60" // درجة شفافية الصورة
-};
+const banners = [
+    {
+        title: "DOLR PLUS",
+        subtitle: "Luxury Digital Services",
+        image: "https://images.unsplash.com/photo-1614850523296-d8c1af93d400?q=80&w=2070",
+        opacity: "opacity-60"
+    },
+    {
+        title: "نجمة السناب",
+        subtitle: "اشتراك رسمي ومضمون",
+        image: "https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?q=80&w=2074",
+        opacity: "opacity-50"
+    }
+];
