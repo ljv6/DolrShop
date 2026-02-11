cfa863df5dc3140e7fc6322af250f7b0a5d8628b
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 02:32:42 +0300

    Update products.js

--- products.js
+++ products.js
@@ -4,31 +4,31 @@ const products = [
         id: 1, 
         name: "شريحة بيانات STC", 
         price: "80.00", 
-        img: "https://i.ibb.co/68XW9QG/IMG-7454.jpg" 
+        img: "IMG_7421.png" 
     },
     { 
         id: 2, 
         name: "اشتراك سناب رسمي (3 أشهر)", 
         price: "25.00", 
-        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Snapchat_logo.svg/1200px-Snapchat_logo.svg.png" 
+        img: "IMG_7416.png" 
     },
     { 
         id: 3, 
         name: "اشتراك سناب رسمي (6 أشهر)", 
         price: "45.00", 
-        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Snapchat_logo.svg/1200px-Snapchat_logo.svg.png" 
+        img: "IMG_7416.png" 
     },
     { 
         id: 4, 
         name: "اشتراك سناب رسمي (12 شهر)", 
         price: "90.00", 
-        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Snapchat_logo.svg/1200px-Snapchat_logo.svg.png" 
+        img: "IMG_7416.png" 
     },
     { 
         id: 5, 
         name: "اشتراك شات جي بي تي (سنة)", 
         price: "35.00", 
-        img: "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg" 
+        img: "IMG_7422.png" 
     }
 ];
 
