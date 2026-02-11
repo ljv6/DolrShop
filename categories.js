617b3992dfbae36160b117ce18e84a062dd056a2
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 02:22:24 +0300

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,14 @@
+const categories = [
+    {
+        name: "سناب شات",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Snapchat_logo.svg/1200px-Snapchat_logo.svg.png"
+    },
+    {
+        name: "ذكاء اصطناعي",
+        img: "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"
+    },
+    {
+        name: "بيانات وإتصال",
+        img: "https://i.ibb.co/68XW9QG/IMG-7454.jpg"
+    }
+];
