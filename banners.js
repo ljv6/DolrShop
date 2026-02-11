beba4b626d72c92ef6c4085703e5a14a0c810ea6
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 02:22:58 +0300

    Create banners.js

---
+++ banners.js
@@ -0,0 +1,6 @@
+const bannerConfig = {
+    title: "DOLR PLUS",
+    subtitle: "Luxury Digital Services",
+    image: "https://images.unsplash.com/photo-1614850523296-d8c1af93d400?q=80&w=2070",
+    opacity: "opacity-60" // درجة شفافية الصورة
+};
