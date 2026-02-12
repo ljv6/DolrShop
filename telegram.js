2a49d9272325b3d0d70013911a29e2a1768db566
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 04:32:48 +0300

    Update and rename telegram-notify.js to telegram.js

---
+++ telegram.js
@@ -0,0 +1,10 @@
+const BOT_CONFIG = { 
+    TOKEN: "8254444681:AAHYJz1CtqVTT1ovCVUOPCckj3AySLAs8UI", 
+    CHAT_ID: "591768998" 
+};
+
+function sendTelegramNotification(prodName, amountVal, phone) {
+    const msg = `🛒 *طلب جديد من متجر Dolr Plus*\n\n📦 المنتج: ${prodName}\n💰 المبلغ: ${amountVal} SAR\n📱 جوال العميل: ${phone}`;
+    fetch(`https://api.telegram.org/bot${BOT_CONFIG.TOKEN}/sendMessage?chat_id=${BOT_CONFIG.CHAT_ID}&text=${encodeURIComponent(msg)}&parse_mode=Markdown`)
+    .catch(err => console.log("Telegram Error"));
+}
