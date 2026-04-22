dbe7650b5f63fe1a6553d9e4d2f329176b3e7c59
ljv6 <maxmohamedmoon@gmail.com>
2026-04-23 01:19:57 +0300

    Add products.js with product listings

---
+++ stu/new/products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    name: "حل واجب",
+    price: 30,
+    img: "https://via.placeholder.com/500x500?text=حل+واجب"
+  },
+  {
+    name: "بحث",
+    price: 60,
+    img: "https://via.placeholder.com/500x500?text=بحث"
+  },
+  {
+    name: "عرض تقديمي",
+    price: 150,
+    img: "https://via.placeholder.com/500x500?text=عرض+تقديمي"
+  }
+];
