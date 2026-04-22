83b4731712e5140550dab303cb63d4a7e1cd76a2
ljv6 <maxmohamedmoon@gmail.com>
2026-04-23 00:02:48 +0300

    Add products.js with initial product data

---
+++ stu/products.js
@@ -0,0 +1,13 @@
+// products.js
+const products = [
+    { 
+        id: 1, 
+        name: "تصميم عرض تقديمي (برزنتيشن)", 
+        price: "150.00",
+        desc: "تصميم احترافي لعرض تقديمي جاهز للتسليم"
+    }
+];
+
+if (typeof module !== 'undefined') {
+    module.exports = products;
+}
