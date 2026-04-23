5600f0fae18f799368e6368c1607b626a32c0a68
ljv6 <maxmohamedmoon@gmail.com>
2026-04-23 03:17:15 +0300

    Update banner subtitle and logo file extension

--- stu/banners.js
+++ stu/banners.js
@@ -1,11 +1,9 @@
 const banners = [
     {
         title: "Dolr Student Services",
-        subtitle: "موثوق في مركز الأعمال السعودي",
-        image: "IMG_7606.jpeg", 
-        logo: "IMG_7454-removebg-preview.pn", // أضفنا مسار اللوجو هنا ليتم استخدامه في المنتصف
+        subtitle: " خـــدمات طــلابيـه ", 
+        logo: "IMG_7454-removebg-preview.png", // أضفنا مسار اللوجو هنا ليتم استخدامه في المنتصف
         opacity: "opacity-80" 
     }
 ];
-
 if (typeof module !== 'undefined') { module.exports = banners; }
