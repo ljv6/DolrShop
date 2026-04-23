8cd86039abedda5897c1cd8483f74e9af5d254ca
ljv6 <maxmohamedmoon@gmail.com>
2026-04-23 03:05:45 +0300

    Add banners.js with banner data structure

---
+++ stu/banners.js
@@ -0,0 +1,11 @@
+const banners = [
+    {
+        title: "DOLR PLUS",
+        subtitle: "موثوق في مركز الأعمال السعودي",
+        image: "IMG_7606.jpeg", 
+        logo: "IMG_7454-removebg-preview.png", // أضفنا مسار اللوجو هنا ليتم استخدامه في المنتصف
+        opacity: "opacity-80" 
+    }
+];
+
+if (typeof module !== 'undefined') { module.exports = banners; }
